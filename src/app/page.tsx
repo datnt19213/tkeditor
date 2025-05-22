64cbd403b4452cf0cf1c9c2f70da17141842ec6a
datnt19213 <datnguyen.frontend.dev@gmail.com>
2025-05-22 18:45:25 +0700

    Update EditorProvider configuration and include Export component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,16 @@ import 'tkeditor/index.css';
 import {
   CoreEditor,
   EditorProvider,
+  Export,
 } from 'tkeditor';
 
 export default function Home() {
   
   return (  
     <div className="p-6 w-full">
-      <EditorProvider >
-        <CoreEditor className='bg-black text-white ' />
+      <EditorProvider isToolbar={false} allowPlugins={[]}  >
+        <Export />
+        <CoreEditor  />
       </EditorProvider>
     </div>
   );
