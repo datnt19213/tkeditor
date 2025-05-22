7ba5d42ef81943217de0f240f9a44361472c8840
datnt19213 <datnguyen.frontend.dev@gmail.com>
2025-05-22 19:12:48 +0700

    Refactor Home component layout and toggle functionality for TKEditor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,29 @@
 "use client";
-
 import 'tkeditor/index.css';
+
+import { useState } from 'react';
 
 import {
   CoreEditor,
   EditorProvider,
   Export,
+  PLUGIN_DEFAULTS,
 } from 'tkeditor';
 
 export default function Home() {
+  const [isFull, setIsFull] = useState(true)
   
   return (  
-    <div className="p-6 w-full">
-      <EditorProvider isToolbar={false} allowPlugins={[]}  >
-        <Export />
-        <CoreEditor  />
+    <div className="p-6 w-full flex-col">
+      <div className="flex px-6 gap-2 py-12 justify-between">
+        <h1 className="text-3xl font-bold">TKEditor</h1>
+        <button type="button" className="min-w-40 text-sm cursor-pointer font-medium px-3 py-2 bg-white border border-gray-500 rounded-md" onClick={() => setIsFull(!isFull)} >
+          {isFull ? 'All' : 'Only Export'}
+        </button>
+      </div>
+      <EditorProvider isToolbar={isFull} allowPlugins={isFull ? PLUGIN_DEFAULTS : []}  >
+        {!isFull && <Export />}
+        <CoreEditor className="bg-gray-50 !px-6" />
       </EditorProvider>
     </div>
   );
